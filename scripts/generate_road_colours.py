8b8f15ab045bcac67f6ad039dcf695dd133e267e
Mateusz Konieczny <matkoniecz@gmail.com>
2015-09-25 18:49:10 +0200

    separate setting and processing data in generate_road_colours.py

--- scripts/generate_road_colours.py
+++ scripts/generate_road_colours.py
@@ -1,11 +1,50 @@
 #!/usr/bin/env python2
-
 from colormath.color_conversions import convert_color
 from colormath.color_objects import LabColor, LCHabColor, sRGBColor
 from colormath.color_diff import delta_e_cie2000
 import numpy
-
 from collections import OrderedDict, namedtuple
+
+
+road_classes = ["motorway", "trunk", "primary", "secondary"]
+
+# The minimum and maximum hue for the road colours
+min_h = 10
+max_h = 106
+
+# A class to hold information for each line
+ColourInfo = namedtuple("ColourInfo", ["start_l", "end_l", "start_c", "end_c"])
+
+line_colour_infos = OrderedDict()
+
+# The saturation and lightness for each type of line
+line_colour_infos["fill"] = ColourInfo(start_l = 70, end_l = 97, start_c = 35, end_c = 29)
+line_colour_infos["low-zoom"] = ColourInfo(start_l = 62, end_l = 92, start_c = 50, end_c = 50)
+
+line_colour_infos["casing"] = ColourInfo(start_l = 50, end_l = 50, start_c = 70, end_c = 55)
+line_colour_infos["low-zoom-casing"] = ColourInfo(start_l = 50, end_l = 70, start_c = 50, end_c = 65)
+
+shield_colour_info = ColourInfo(start_l = 65, end_l = 80, start_c = 30, end_c = 30)
+colourless_roads = ["teriary"]
+
+
+
+
+#Start processing set data
+colour_divisions = len(road_classes) - 1
+hues = OrderedDict()
+
+# Because hue is a circle, it may be needed to add/subtract 360 to the min or
+# max when changing them
+delta_h = (max_h - min_h) / colour_divisions
+
+h = min_h
+for name in road_classes:
+  hues[name] = h
+  h = (h + delta_h) % 360
+
+# Colours for the MSS
+colours = OrderedDict()
 
 class Color:
   def __init__(self, lch_tuple):
@@ -24,39 +63,6 @@ class Color:
     return delta_e_cie2000(convert_color(self.m_lch, LabColor),
                            convert_color(sRGBColor.new_from_rgb_hex(self.rgb()), LabColor))
 
-road_classes = ["motorway", "trunk", "primary", "secondary"]
-colour_divisions = len(road_classes) - 1
-hues = OrderedDict()
-
-# The minimum and maximum hue for the road colours
-# Because hue is a circle, it may be needed to add/subtract 360 to the min or
-# max when changing them
-
-min_h = 10
-max_h = 106
-delta_h = (max_h - min_h) / colour_divisions
-
-h = min_h
-for name in road_classes:
-  hues[name] = h
-  h = (h + delta_h) % 360
-
-print hues
-# A class to hold information for each line
-ColourInfo = namedtuple("ColourInfo", ["start_l", "end_l", "start_c", "end_c"])
-
-line_colour_infos = OrderedDict()
-
-# The saturation and lightness for each type of line
-line_colour_infos["fill"] = ColourInfo(start_l = 70, end_l = 97, start_c = 35, end_c = 29)
-line_colour_infos["low-zoom"] = ColourInfo(start_l = 62, end_l = 92, start_c = 50, end_c = 50)
-
-line_colour_infos["casing"] = ColourInfo(start_l = 50, end_l = 50, start_c = 70, end_c = 55)
-line_colour_infos["low-zoom-casing"] = ColourInfo(start_l = 50, end_l = 70, start_c = 50, end_c = 65)
-
-# Colours for the MSS
-colours = OrderedDict()
-
 for line_name, line_colour_info in line_colour_infos.iteritems():
   c = line_colour_info.start_c
   delta_c = (line_colour_info.end_c - line_colour_info.start_c) / colour_divisions
@@ -72,7 +78,6 @@ for name, colour in colours.iteritems():
   print "@{name}: {rgb}; // {lch}, error {delta:.1f}".format(name = name, rgb = colour.rgb(), lch = colour.lch(), delta = colour.rgb_error())
 
 # Generate colours for the shields
-shield_colour_info = ColourInfo(start_l = 65, end_l = 80, start_c = 30, end_c = 30)
 shield_colours = OrderedDict()
 
 
@@ -86,7 +91,8 @@ for name in road_classes:
   c += delta_c
   l += delta_l
 
-shield_colours["tertiary"] = Color((shield_colour_info.end_l, 0, 0))
+for road in colourless_roads:
+  shield_colours[road] = Color((shield_colour_info.end_l, 0, 0))
 
 print "\n\nRoad shield information\n\n"
 for name, colour in shield_colours.iteritems():
