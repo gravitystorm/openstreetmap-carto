360a94ae209df8176db730b82bcd43764fb0d03c
Asbjørn Sloth Tønnesen <asbjorn@asbjorn.st>
2016-04-18 19:38:24 +0000

    move project.yaml to project.d/
    Also extend yaml2mml.py to merge all yaml files under project.d
    
    Signed-off-by: Asbjørn Sloth Tønnesen <asbjorn@asbjorn.st>

--- scripts/yaml2mml.py
+++ scripts/yaml2mml.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import argparse, json, os, sys, yaml
+from six import iteritems
+import argparse, json, os, sys, yaml, glob
 
-parser = argparse.ArgumentParser(description='Keeps project files in sync by converting project.yaml to project.mml.')
+def merge(dst, src):
+  if src == None:
+    return
+  if isinstance(src, dict):
+    for k,v in iteritems(src):
+      if k in dst:
+        merge(dst.get(k), v)
+      else:
+        dst[k] = v
+  else:
+    if dst != src:
+      dst += src
+
+parser = argparse.ArgumentParser(description='Keeps project files in sync by converting project.d/*.yaml to project.mml.')
 parser.add_argument('--check', dest='check', help='write generated JSON to stdout instead to project.mml', required=False, action='store_true', default=False)
 args = parser.parse_args()
 
-yaml_path = os.path.join(os.path.dirname(__file__), '../project.yaml')
-mml_path = os.path.join(os.path.dirname(__file__), '../project.mml')
+script_dir = os.path.dirname(__file__)
+yaml_pattern = os.path.join(script_dir, '../project.d/*.yaml')
+mml_path = os.path.join(script_dir, '../project.mml')
 
-yaml_data = None
+yaml_data = {}
 
 try:
-  yaml_file = open(yaml_path)
-  yaml_data = yaml.safe_load(yaml_file)
-  yaml_file.close()
-
+  yaml_files = glob.glob(yaml_pattern)
+  yaml_files.sort()
+  for filename in yaml_files:
+    yaml_stream = open(filename, 'rb')
+    partial_data = yaml.safe_load(yaml_stream)
+    merge(yaml_data, partial_data)
+    yaml_stream.close()
 except IOError:
   print('Could not read YAML file. Aborting.')
   sys.exit(1)
