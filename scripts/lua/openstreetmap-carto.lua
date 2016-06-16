4499350d2673770abf7e16a3a1a5dd6cdf440264
Paul Norman <penorman@mac.com>
2016-06-15 18:05:44 -0700

    Add testing framework

---
+++ scripts/lua/openstreetmap-carto.lua
@@ -0,0 +1,1 @@
+../../openstreetmap-carto.lua
---
+++ scripts/lua/test.lua
@@ -0,0 +1,21 @@
+--[[
+This file is part of OpenStreetMap Carto and used for validating the Lua tag transforms.
+
+Run it with lua test.lua
+]]
+
+require ("openstreetmap-carto")
+
+print("TESTING: z_order")
+
+assert(z_order({}) == nil, "test failed: no tags")
+assert(z_order({foo="bar"}) == nil, "test failed: other tags")
+assert(z_order({highway="motorway"}) == 380 , "test failed: motorway")
+assert(z_order({highway="motorway", railway="rail"}) == 440 , "test failed: motorway + rail")
+
+print("TESTING: roads")
+assert(roads({}) == 0, "test failed: no tags")
+assert(roads({foo="bar"}) == 0, "test failed: other tags")
+assert(roads({highway="motorway"}) == 1, "test failed: motorway")
+assert(roads({railway="rail"}) == 1, "test failed: rail")
+assert(roads({highway="residential", railway="rail"}) == 1, "test failed: rail")
