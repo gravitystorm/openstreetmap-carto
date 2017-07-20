dab2eda2799b1f4b16d3a2f4a2eb993549184caf
Daniel Koć <daniel@xn--ko-wla.pl>
2017-07-20 21:25:52 +0200

    Fixing .env file creation for Docker

--- scripts/docker-startup.sh
+++ scripts/docker-startup.sh
@@ -3,6 +3,7 @@
 # This script is used to start the import or kosmtik containers for the Docker development environment.
 # You can read details about that in DOCKER.md
 
+# Testing if database is ready
 i=1
 MAXCOUNT=60
 echo "Waiting for PostgreSQL to be running"
@@ -16,9 +17,27 @@ test $i -gt $MAXCOUNT && echo "Timeout while waiting for PostgreSQL to be runnin
 
 case "$1" in
 import)
+  # Creating default database
   psql -c "SELECT 1 FROM pg_database WHERE datname = 'gis';" | grep -q 1 || createdb gis && \
   psql -d gis -c 'CREATE EXTENSION IF NOT EXISTS postgis;' && \
   psql -d gis -c 'CREATE EXTENSION IF NOT EXISTS hstore;' && \
+
+  # Creating default import settings file editable by user and passing values for osm2pgsql
+  if [ ! -e ".env" ]; then
+    cat > .env <<EOF
+# Environment settings for importing to a Docker container database
+
+OSM2PGSQL_CACHE=512
+OSM2PGSQL_NUMPROC=1
+OSM2PGSQL_DATAFILE=data.osm.pbf
+EOF
+    chmod a+rw .env
+    export OSM2PGSQL_CACHE=512
+    export OSM2PGSQL_NUMPROC=1
+    export OSM2PGSQL_DATAFILE=data.osm.pbf
+  fi
+
+  # Importing data to a database
   osm2pgsql \
   --cache $OSM2PGSQL_CACHE \
   --number-processes $OSM2PGSQL_NUMPROC \
@@ -29,15 +48,20 @@ import)
   --tag-transform-script openstreetmap-carto.lua \
   $OSM2PGSQL_DATAFILE
   ;;
+
 kosmtik)
+  # Downloading needed shapefiles
   python scripts/get-shapefiles.py -n
 
+  # Creating default Kosmtik settings file
   if [ ! -e ".kosmtik-config.yml" ]; then
     cp /tmp/.kosmtik-config.yml .kosmtik-config.yml  
   fi
   export KOSMTIK_CONFIGPATH=".kosmtik-config.yml"
 
+  # Starting Kosmtik
   kosmtik serve project.mml --host 0.0.0.0
-  # It needs Control C to be interrupted
+  # It needs Ctrl+C to be interrupted
   ;;
+
 esac
