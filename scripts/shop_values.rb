ca8d85314469a0443c3a524a457ae0bdbd7f51ef
Mateusz Konieczny <matkoniecz@gmail.com>
2019-03-25 15:33:08 +0100

    include known OK ones to make updates easier

--- scripts/shop_values.rb
+++ scripts/shop_values.rb
@@ -44,6 +44,49 @@ EXCEPTIONS = [
    "business_machines", # duplicate of shop=electronics
 ]
 
+KNOWN_GOOD_VALUES = ["convenience", "supermarket", "clothes", "hairdresser",
+  "bakery", "car_repair", "car", "kiosk", "beauty", "butcher",
+  "hardware", "furniture", "florist", "mobile_phone", "electronics", "mall",
+  "shoes", "alcohol", "doityourself", "car_parts", "jewelry", "optician",
+  "books", "department_store", "gift", "bicycle", "greengrocer",
+  "variety_store", "travel_agency", "sports", "confectionery", "laundry",
+  "computer", "chemist", "stationery", "pet", "beverages", "newsagent",
+  "dry_cleaning", "tyres", "motorcycle", "garden_centre", "toys", "copyshop",
+  "cosmetics", "boutique", "funeral_directors", "tailor", "deli", "tobacco",
+  "seafood", "farm", "interior_decoration", "photo", "ticket", "outdoor",
+  "massage", "art", "houseware", "wine", "paint", "fabric", "trade",
+  "bookmaker", "kitchen", "second_hand", "lottery", "pastry", "pawnbroker",
+  "fashion", "bed", "charity", "tattoo", "antiques", "craft", "medical_supply",
+  "hifi", "music", "coffee", "musical_instrument", "gas", "estate_agent",
+  "video", "baby_goods", "tea", "hearing_aids", "bag", "perfumery",
+  "carpet", "video_games", "curtain", "dairy", "erotic", "money_lender",
+  "locksmith", "cheese", "frame", "wholesale", "bathroom_furnishing",
+  "chocolate", "appliance", "e-cigarette", "ice_cream", "food",
+  "herbalist", "electrical", "fishing", "watches", "nutrition_supplements",
+  "radiotechnics", "agrarian", "money_transfer", "motorcycle_repair",
+  "glaziery", "religion", "leather", "scuba_diving", "grocery", "sewing",
+  "water", "health_food", "lighting", "weapons", "shoe_repair", "fishmonger",
+  "tiles", "frozen_food", "photo_studio", "pet_grooming", "printing", "party",
+  "building_materials", "flooring", "window_blind", "games",
+  "hobby", "country_store", "vacuum_cleaner", "fuel", "hunting", "rental",
+  "doors", "cannabis", "furnace", "pyrotechnics", "boat", "lamps", "fireplace",
+  "swimming_pool", "accessories", "household", "water_sports", "model",
+  "haberdashery", "pottery", "spices", "pasta", "jewellery",
+  "hairdresser_supply", "discount", "office_supplies", "solarium", "candles",
+  "camera", "caravan", "outpost",
+  "souvenir", "anime", "catalogue", "collector",
+  "communication", "car_service", "health", "energy", "security", "wood",
+  "stationary", "trophy", "honey", "windows", "wool", "wedding", "ski",
+  "storage_units", "tanning", "salon", "photography", "rice", "cafe",
+  "glass", "bathroom", "garden_furniture", "golf", "medical", "tool_hire",
+  "construction", "tableware", "signs", "fireworks", "printer_ink",
+  "household_linen", "plumber", "plumbing", "beauty_salon", "groundskeeping",
+  "print", "builder", "card", "gallery", "ship_chandler",
+  "supplements", "agriculture", "chandler", "canoe_hire", "wigs", "nuts",
+  "hvac", "equestrian", "military_surplus", "brewing_supplies", "trailer",
+  "atv", "power_tools",  "hookah", "electrotools", "maps", "snowmobile",
+  "açaí", "jetski", "vehicles", "mobile_home", "free_flying", "junk_yard",
+]
 
 data = URI.parse('https://taginfo.openstreetmap.org/api/4/key/values?key=shop&sortname=count&sortorder=desc').read
 data = JSON.parse(data)["data"]
@@ -62,6 +105,10 @@ candidates = no_empty + on_wiki
 # Filter out exceptions in EXCEPTIONS
 filtered = candidates - EXCEPTIONS
 
+unknown_status = filtered - KNOWN_GOOD_VALUES
+if unknown_status != []
+  puts "unexpected, possible wanted values: #{unknown_status}"
+end
 
 # Output in SQL style
 puts "(" + filtered.map{ |val| "'#{val}'" }.sort.join(", ") + ")"
