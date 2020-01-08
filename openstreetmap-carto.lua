094aa5d9de244fbd1a3b0078879a8cb3cecbf26e
Joseph E <42757252+jeisenbe@users.noreply.github.com>
2020-01-09 07:24:48 +0900

    Import man_made=pipeline, man_made=cutline and power=cable as linestrings (#3973)
    * Add power=cable, man_made=cutline and man_made=pipeline as a linestring only features in the lua file
    - These features will now be imported only as linestrings, not as polygons, when tagged on a closed way.

--- openstreetmap-carto.lua
+++ openstreetmap-carto.lua
@@ -37,9 +37,9 @@ local polygon_keys = {
 local linestring_values = {
     historic = {citywalls = true},
     leisure = {track = true, slipway = true},
-    man_made = {embankment = true, breakwater = true, groyne = true},
+    man_made = {breakwater = true, cutline = true, embankment = true, groyne = true, pipeline = true},
     natural = {cliff = true, tree_row = true, ridge = true, arete = true},
-    power = {line = true, minor_line = true},
+    power = {cable = true, line = true, minor_line = true},
     tourism = {yes = true},
     waterway = {canal = true, derelict_canal = true, ditch = true, drain = true, river = true, stream = true, wadi = true, weir = true}
 }
