a3712a8dec0f8428ac0b18a8cbc794b5a8eb5d1a
jeisenbe <joseph.eisenberg@gmail.com>
2020-01-30 12:57:09 +0900

    Import golf closed ways as polygons, with exceptions
    Add golf key to list of polygon keys in openstreetmap-carto.lua so that closed ways with this key will be imported as polygons, except for golf=hole, golf=path and golf=cartpath which are always mapped as lines

--- openstreetmap-carto.lua
+++ openstreetmap-carto.lua
@@ -14,6 +14,7 @@ local polygon_keys = {
     'area:highway',
     'building',
     'building:part',
+    'golf',
     'harbour',
     'healthcare',
     'historic',
@@ -35,6 +36,7 @@ local polygon_keys = {
 
 -- Objects with any of the following key/value combinations will be treated as linestring
 local linestring_values = {
+    golf = {cartpath = true, hole = true, path = true}, 
     historic = {citywalls = true},
     leisure = {track = true, slipway = true},
     man_made = {breakwater = true, cutline = true, embankment = true, groyne = true, pipeline = true},
