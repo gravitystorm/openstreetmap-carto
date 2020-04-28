bc117f6d65c4f881df2c119f378264aa185da229
Paul Norman <penorman@mac.com>
2020-04-27 19:29:42 -0700

    Move the shapefile import to the import docker container

--- scripts/docker-startup.sh
+++ scripts/docker-startup.sh
@@ -50,12 +50,12 @@ EOF
   --style openstreetmap-carto.style \
   --tag-transform-script openstreetmap-carto.lua \
   $OSM2PGSQL_DATAFILE
+
+  # Downloading needed shapefiles
+  scripts/get-external-data.py
   ;;
 
 kosmtik)
-  # Downloading needed shapefiles
-  scripts/get-external-data.py
-
   # Creating default Kosmtik settings file
   if [ ! -e ".kosmtik-config.yml" ]; then
     cp /tmp/.kosmtik-config.yml .kosmtik-config.yml
