b9ad82a489d2fd3097115fa8bc4f0e247a5b02e9
Nick Doiron <ndoiron@mapmeld.com>
2023-10-28 12:52:36 -0500

    Use notofonts.github.io for newer font releases

--- scripts/get-fonts.sh
+++ scripts/get-fonts.sh
@@ -17,70 +17,70 @@ download() {
 REGULAR_BOLD_ITALIC="NotoSans"
 
 # Fonts available in regular and bold
-REGULAR_BOLD="NotoSansAdlamUnjoined
-NotoSansArabicUI
-NotoSansArmenian
-NotoSansBalinese
-NotoSansBamum
-NotoSansBengaliUI
-NotoSansCanadianAboriginal
-NotoSansCham
-NotoSansCherokee
-NotoSansDevanagariUI
-NotoSansEthiopic
-NotoSansGeorgian
-NotoSansGujaratiUI
-NotoSansGurmukhiUI
-NotoSansHebrew
-NotoSansJavanese
-NotoSansKannadaUI
-NotoSansKayahLi
-NotoSansKhmerUI
-NotoSansLaoUI
-NotoSansLisu
-NotoSansMalayalamUI
-NotoSansMyanmarUI
-NotoSansOlChiki
-NotoSansOriyaUI
-NotoSansSinhalaUI
-NotoSansSundanese
-NotoSansSymbols
-NotoSansTaiTham
-NotoSansTamilUI
-NotoSansTeluguUI
-NotoSansThaana
-NotoSansThaiUI
-NotoSerifTibetan"
+REGULAR_BOLD="NotoSansAdlamUnjoined;adlam
+NotoSansArabicUI;arabic
+NotoSansArmenian;armenian
+NotoSansBalinese;balinese
+NotoSansBamum;bamum
+NotoSansBengaliUI;bengali
+NotoSansCanadianAboriginal;canadian-aboriginal
+NotoSansCham;cham
+NotoSansCherokee;cherokee
+NotoSansDevanagariUI;devanagari
+NotoSansEthiopic;ethiopic
+NotoSansGeorgian;georgian
+NotoSansGujaratiUI;gujarati
+NotoSansGurmukhiUI;gurmukhi
+NotoSansHebrew;hebrew
+NotoSansJavanese;javanese
+NotoSansKannadaUI;kannada
+NotoSansKayahLi;kayah-li
+NotoSansKhmerUI;khmer
+NotoSansLaoUI;lao
+NotoSansLisu;lisu
+NotoSansMalayalamUI;malayalam
+NotoSansMyanmarUI;myanmar
+NotoSansOlChiki;ol-chiki
+NotoSansOriya;oriya
+NotoSansSinhalaUI;sinhala
+NotoSansSundanese;sundanese
+NotoSansSymbols;symbols
+NotoSansTaiTham;tai-tham
+NotoSansTamilUI;tamil
+NotoSansTeluguUI;telugu
+NotoSansThaana;thaana
+NotoSansThaiUI;thai
+NotoSerifTibetan;tibetan"
 
 # Fonts with regular and black, but no bold
-REGULAR_BLACK="NotoSansSyriac"
+REGULAR_BLACK="NotoSansSyriac;syriac"
 
 # Fonts only available in regular
-REGULAR="NotoSansBatak
-NotoSansBuginese
-NotoSansBuhid
-NotoSansChakma
-NotoSansCoptic
-NotoSansHanunoo
-NotoSansLepcha
-NotoSansLimbu
-NotoSansMandaic
-NotoSansMongolian
-NotoSansNewTaiLue
-NotoSansNKo
-NotoSansOsage
-NotoSansOsmanya
-NotoSansSamaritan
-NotoSansSaurashtra
-NotoSansShavian
-NotoSansSymbols2
-NotoSansTagalog
-NotoSansTagbanwa
-NotoSansTaiLe
-NotoSansTaiViet
-NotoSansTifinagh
-NotoSansVai
-NotoSansYi"
+REGULAR="NotoSansBatak;batak
+NotoSansBuginese;buginese
+NotoSansBuhid;buhid
+NotoSansChakma;chakma
+NotoSansCoptic;coptic
+NotoSansHanunoo;hanunoo
+NotoSansLepcha;lepcha
+NotoSansLimbu;limbu
+NotoSansMandaic;mandaic
+NotoSansMongolian;mongolian
+NotoSansNewTaiLue;new-tai-lue
+NotoSansNKo;nko
+NotoSansOsage;osage
+NotoSansOsmanya;osmanya
+NotoSansSamaritan;samaritan
+NotoSansSaurashtra;saurashtra
+NotoSansShavian;shavian
+NotoSansSymbols2;symbols
+NotoSansTagalog;tagalog
+NotoSansTagbanwa;tagbanwa
+NotoSansTaiLe;tai-le
+NotoSansTaiViet;tai-viet
+NotoSansTifinagh;tifinagh
+NotoSansVai;vai
+NotoSansYi;yi"
 
 # Download the fonts in the lists above
 
@@ -88,28 +88,46 @@ for font in $REGULAR_BOLD_ITALIC; do
   regular="$font-Regular.ttf"
   bold="$font-Bold.ttf"
   italic="$font-Italic.ttf"
-  download "${FONTDIR}/${regular}" "https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/${font}/${regular}"
-  download "${FONTDIR}/${bold}" "https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/${font}/${bold}"
-  download "${FONTDIR}/${italic}" "https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/${font}/${italic}"
+  download "${FONTDIR}/${regular}" "https://notofonts.github.io/latin-greek-cyrillic/fonts/${font}/hinted/ttf/${regular}"
+  download "${FONTDIR}/${bold}" "https://notofonts.github.io/latin-greek-cyrillic/fonts/${font}/hinted/ttf/${bold}"
+  download "${FONTDIR}/${italic}" "https://notofonts.github.io/latin-greek-cyrillic/fonts/${font}/hinted/ttf/${italic}"
 done
 
-for font in $REGULAR_BOLD; do
+for fontDir in $REGULAR_BOLD; do
+  IFS=";" read -r -a arr <<< "${fontDir}"
+  font="${arr[0]}"
+  subfolder="${arr[1]}"
   regular="$font-Regular.ttf"
   bold="$font-Bold.ttf"
-  download "${FONTDIR}/${regular}" "https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/${font}/${regular}"
-  download "${FONTDIR}/${bold}" "https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/${font}/${bold}"
+  # remove UI from South Asian scripts path (not consistent with Arabic, Khmer, Thai)
+  font="${font/BengaliUI/Bengali}"
+  font="${font/GujaratiUI/Gujarati}"
+  font="${font/GurmukhiUI/Gurmukhi}"
+  font="${font/KannadaUI/Kannada}"
+  font="${font/MalayalamUI/Malayalam}"
+  font="${font/SinhalaUI/Sinhala}"
+  font="${font/TamilUI/Tamil}"
+  font="${font/TeluguUI/Telugu}"
+  download "${FONTDIR}/${regular}" "https://notofonts.github.io/${subfolder}/fonts/${font}/hinted/ttf/${regular}"
+  download "${FONTDIR}/${bold}" "https://notofonts.github.io/${subfolder}/fonts/${font}/hinted/ttf/${bold}"
 done
 
-for font in $REGULAR_BLACK; do
+for fontDir in $REGULAR_BLACK; do
+  IFS=";" read -r -a arr <<< "${fontDir}"
+  font="${arr[0]}"
+  subfolder="${arr[1]}"
   regular="$font-Regular.ttf"
   black="$font-Black.ttf"
-  download "${FONTDIR}/${regular}" "https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/${font}/${regular}"
-  download "${FONTDIR}/${black}" "https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/${font}/${black}"
+  download "${FONTDIR}/${regular}" "https://notofonts.github.io/${subfolder}/fonts/${font}/hinted/ttf/${regular}"
+  download "${FONTDIR}/${black}" "https://notofonts.github.io/${subfolder}/fonts/${font}/hinted/ttf/${black}"
 done
 
-for font in $REGULAR; do
+for fontDir in $REGULAR; do
+  IFS=";" read -r -a arr <<< "${fontDir}"
+  font="${arr[0]}"
+  subfolder="${arr[1]}"
   regular="$font-Regular.ttf"
-  download "${FONTDIR}/${regular}" "https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/${font}/${regular}"
+  download "${FONTDIR}/${regular}" "https://notofonts.github.io/${subfolder}/fonts/${font}/hinted/ttf/${regular}"
 done
 
 # Other noto fonts which don't follow the URL pattern above
